2b572b69f2c5bf879276c91edc11bf12b1bb44cc
higzo <ashrafhgazy9@gmail.com>
2025-06-30 11:33:14 +0300

    code

---
+++ Assignment 3/Assignment 3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.Write("Enter Number To Check if able to divided by '3'and'4' : ");
+            //bool Flag = int.TryParse(Console.ReadLine(), out int Number);
+
+            //if (Flag && Number != 0)
+            //{
+            //    if (Number % 3 == 0 && Number % 4 == 0)
+            //    {
+            //        Console.WriteLine("Yes");
+            //    }
+            //    else { Console.WriteLine("No"); }
+
+            //}
+            //else { Console.WriteLine("Invalid input"); } 
+            #endregion
+            #region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            //Console.Write("Enter Number To Check if negative or Positive number : ");
+            //bool flag = int.TryParse(Console.ReadLine(), out int number);
+
+            //if (flag && number != 0)
+            //{
+            //    if (number > 0)
+            //    {
+            //        Console.WriteLine("positive");
+            //    }
+            //    else if (number < 0)
+            //    {
+            //        Console.WriteLine("Negative");
+            //    }
+            //}
+            //else { Console.WriteLine("Invalid input"); } 
+            #endregion
+            #region 3- Write a program that takes 3 integers from the user then prints the max element and the min element.
+            //Console.WriteLine("Enter Three Numbers  : ");
+            //bool flag1 = int.TryParse(Console.ReadLine(), out int num1);
+            //bool flag2 = int.TryParse(Console.ReadLine(), out int num2);
+            //bool flag3 = int.TryParse(Console.ReadLine(), out int num3);
+
+            //if (flag1 && flag2 && flag3)/*&&num1 != 0 && num2 != 0 && num3*/
+            //{
+            //    int max = num1;
+            //    if (max < num2) max = num2;
+            //    if (max < num3) max = num3;
+
+            //    int min = num1;
+            //    if (min > num2) min = num2;
+            //    if (min > num3) min = num3;
+
+            //    Console.WriteLine($"Max element = {max}");
+            //    Console.WriteLine($"Nin element = {min}");
+            //} 
+            #endregion
+            #region 4- Write a program that allows the user to insert an integer number then check If a number is even or odd.
+            //Console.Write("Enter Number To Check if Even or Odd number : ");
+            //bool flag = int.TryParse(Console.ReadLine(), out int number);
+            //if (flag)
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.WriteLine("Even Number");
+            //    }
+            //    else if (number % 2 != 0)
+            //    {
+            //        Console.WriteLine("Odd Number");
+            //    }
+            //} 
+            #endregion
+            #region 5- Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+            //Console.Write("Enter Character To Check if vowel or not : ");
+            //string input = Console.ReadLine();
+
+            //if (input is not null)
+            //{
+            //    char c = char.ToLower(input[0]);
+            //    switch (c)
+            //    {
+            //        case 'a':
+            //        case 'e':
+            //        case 'i':
+            //        case 'o':
+            //        case 'u':
+            //            Console.WriteLine("Vowel");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Not a vowel");
+            //            break;
+            //    } 
+            //}
+            #endregion
+
+        }
+    }
+}
